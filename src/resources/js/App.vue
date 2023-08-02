33cfd10dbb666f6d57b3f5635cf83db4d87a41c5
shuichi0712 <shu1good@gmail.com>
2023-08-02 09:57:21 +0900

    first commit

---
+++ src/resources/js/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+const test: number = 1;
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <div>{{ test }}</div>
+</template>
